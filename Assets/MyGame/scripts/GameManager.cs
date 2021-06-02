53da4ddf0880965fdff16b049582990fcac2385d
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-02 16:58:49 +0200

    SceneLoader Script

---
+++ Assets/MyGame/scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    private InputField userName;
+    private Text tmpName;
+
+    public NameData nameData;
+    public SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        userName = FindObjectOfType<InputField>();
+    }
+
+    public void SaveName()
+    {
+        nameData.name = "NameData";
+        nameData.yourName = userName.text;
+        sceneLoader.LoadNextScene();
+    }
+
+    public void LoadName()
+    {
+        if(tmpName == null)
+        {
+            tmpName = GameObject.Find("SavedName").GetComponent<Text>();
+        }
+
+        tmpName.text = nameData.yourName;
+    }
+}
